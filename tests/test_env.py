3a22cf1539a5f2f27b23b9ba5339829304e4e1c7
Tom Dörr <tomdoerr96@gmail.com>
2025-03-16 08:46:31 +0100

    test: Add test_env.py with basic Env1 tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -4,7 +4,7 @@ from src import *
 
 def test_env_1():
     env_1 = Env1()
-    assert env_1('aaa') == 3
-    assert env_1('abjkladfaa') == 4
-    assert env_1('abjkldfa') == 2
-    assert env_1('bjkldf') == 0
+    assert env_1("aaa") == 3
+    assert env_1("abjkladfaa") == 4
+    assert env_1("abjkldfa") == 2
+    assert env_1("bjkldf") == 0
