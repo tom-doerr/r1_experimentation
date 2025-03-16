4f4fbcf72e4ab6d246714faa70dff64c92290b26
Tom Dörr <tomdoerr96@gmail.com>
2025-03-16 08:50:54 +0100

    feat: Add Env1 tests with penalty start and target char

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,9 +1,13 @@
 import pytest
-from src import Env1
+from src import *
 
 
-def test_env():
-    env_1 = Env1()
+def test_env_1():
+    env_1 = Env1(target_char="a", char_count_penalty_start=10)
     assert env_1("aaa") == 3
-    assert env_1("abjkladfaa") == 4
+    assert env_1("abjdfaa") == 4
     assert env_1("abjkldfa") == 2
+    assert env_1("bjkldf") == 0
+    assert env_1("bjkldfjdfdj") == -1
+    assert env_1("bakldfjdfdj") == 0
+    assert env_1("bakldfjdfda") == -1
