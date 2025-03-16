72386f32f2b82e30c90a5cdc5177a95f434ffb07
Tom Dörr <tomdoerr96@gmail.com>
2025-03-16 10:03:11 +0100

    test: Add tests for Env1 environment

--- tests/test_env.py
+++ tests/test_env.py
@@ -5,7 +5,7 @@ from src.envs import *
 def test_env_1():
     env_1 = Env1(target_char="a", char_count_penalty_start=10)
     assert env_1("aaa") == 3
-    assert env_1("abjdfaa") == 4
+    assert env_1("abajdfaa") == 4
     assert env_1("abjkldfa") == 2
     assert env_1("bjkldf") == 0
     assert env_1("bjkldfjdfdj") == -1
