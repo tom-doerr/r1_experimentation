5c74df6c30c32f00a2a7e5dd8ed9290e27967308
Tom Dörr <tomdoerr96@gmail.com>
2025-03-16 09:40:36 +0100

    .

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,4 +1,5 @@
-from src import Env1
+import pytest
+from src.envs import *
 
 
 def test_env_1():
@@ -10,3 +11,8 @@ def test_env_1():
     assert env_1("bjkldfjdfdj") == -1
     assert env_1("bakldfjdfdj") == 0
     assert env_1("bakldfjdfda") == -1
+
+def test_env_1_defaults():
+    env_1 = Env1()
+    assert env_1.target_char == "a"
+    assert env_1.char_count_penalty_start == 23
