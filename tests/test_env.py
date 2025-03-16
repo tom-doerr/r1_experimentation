57f6a459db5b3eeef98a38446adb7d678620071f
Tom Dörr <tomdoerr96@gmail.com>
2025-03-16 08:33:22 +0100

    .

---
+++ tests/test_env.py
@@ -0,0 +1,10 @@
+import pytest
+from src import *
+
+
+def test_env_1():
+    env_1 = Env1()
+    assert env_1('aaa') == 3
+    assert env_1('abjkladfaa') == 4
+    assert env_1('abjkldfa') == 2
+    assert env_1('bjkldf') == 0
