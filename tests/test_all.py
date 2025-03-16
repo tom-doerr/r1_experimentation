9d31e6dc3a271bb531e75142932c5c828d0e83e7
Tom Dörr <tomdoerr96@gmail.com>
2025-03-16 08:25:53 +0100

    feat: add tests for core functionalities and agent interactions

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,20 +1,19 @@
 import os
 import sys
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import src
 from src import *
 from src.main import *
-from src.main import ShellCodeExecutor
 
-FLASH = 'openrouter/google/gemini-2.0-flash-001'
+
+FLASH = 'openrouter/google/gemini-2.0-flash-001'  
 R1 = 'deepseek/deepseek-reasoner'
 OR1 = 'openrouter/deepseek/deepseek-r1'
 MODEL = FLASH
 
-XML_DATA = '<response><message>hello</message></response>'
-parsed_data = parse_xml(XML_DATA)
+xml_data = '<response><message>hello</message></response>'
+parsed_data = parse_xml(xml_data)
 
 message = parsed_data['message']
 print("message:", message)
@@ -30,9 +29,13 @@ print("reply_generator:", reply_generator)
 for reply in reply_generator:
     print("reply:", reply, end='')
 
+
+
 test_output_var = python_reflection_testing()
 print("test_output_var:", test_output_var)
 assert test_output_var == 'test_output_var'
+
+
 
 reward = test_env_1('aaa')
 assert reward == 3
@@ -40,10 +43,13 @@ assert reward == 3
 reward = test_env_1('aabbjadfa')
 assert reward == 4
 
+
+
 reply_generator = litellm_streaming('hi', model=OR1, max_tokens=40)
 print("reply_generator:", reply_generator)
 for reply in reply_generator:
     print("-", reply, end='')
+
 
 agent = Agent(model=MODEL)
 
@@ -59,17 +65,19 @@ xml_data_2 = '<response><thinking>test abc def</thinking><message>Hi! How can I 
 parsed_data_2 = agent._parse_xml(xml_data_2)
 assert parsed_data_2['message'] == 'Hi! How can I help you?'
 assert parsed_data_2['thinking'] == 'test abc def'
-assert parsed_data_2['memory']['search'] == ""
+assert parsed_data_2['memory']['search'] == ''
 assert parsed_data_2['memory']['replace'] == 'The user wrote just hi.'
 
 agent._update_memory(parsed_data_2['memory']['search'], parsed_data_2['memory']['replace'])
-assert agent.memory == "The user wrote just hi."
+assert agent.memory == 'The user wrote just hi.'
+
 
 agent_assert = AgentAssert(model=MODEL)
-assert isinstance(agent_assert.agent, Agent)
+assert type(agent_assert.agent) == Agent
 
 bool_val = agent_assert._parse_xml('<response><message>The implementation does not match specifications</message><bool>False</bool></response>')
 assert bool_val == False
+
 
 return_val = agent_assert('twenty two has has the same meaning as 22')
 print("return_val:", return_val)
@@ -79,12 +87,15 @@ two_plus_two_is_4 = agent_assert('two plus two is 5')
 print("two_plus_two_is_4:", two_plus_two_is_4)
 assert two_plus_two_is_4 == False
 
+
 shell_code_executor = ShellCodeExecutor()
-assert isinstance(shell_code_executor, ShellCodeExecutor)
+assert type(shell_code_executor) == Tool
+
 
 # check if this is a subset of the blacklisted commands
 assert {'rm', 'cat', 'mv', 'cp'} & set(shell_code_executor.blacklisted_commands) == {'rm', 'cat', 'mv', 'cp'}
 assert {'ls', 'date'} & set(shell_code_executor.whitelisted_commands) == {'ls', 'date'}
+
 
 shell_code_executor_ls = shell_code_executor('ls')
 print("shell_code_executor_ls:", shell_code_executor_ls)
